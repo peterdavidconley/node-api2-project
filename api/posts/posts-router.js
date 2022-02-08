c95619e22dd0f4711ba087347bdc2d0eefdbb9b0
Peter Conley <peterdavidconley@gmail.com>
2022-02-08 13:37:56 -0500

    imported router to server.js, prepped to write endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,9 @@
 // implement your posts router here
+
+const express = require('express')
+const router = express.Router();
+const Posts = require('./posts-model');
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,10 +2,14 @@
 
 const express = require('express');
 const server = express();
+const postsRouter = require('./posts/posts-router');
 
 server.use(express.json());
 
 // require your posts router and connect it here
+
+server.use('/api/posts', postsRouter)
+
 
 server.get('/', (req, res) => {
     res.status(200).json({
