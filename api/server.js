c71addb2df3c8545e7fe119b12c44efb4d2044d8
Peter Conley <peterdavidconley@gmail.com>
2022-02-08 13:20:04 -0500

    wrote base files for index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
+
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
 // require your posts router and connect it here
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'root access working'
+    })
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it here
+
+const server = require('./api/server');
+
+const port = 6000;
+
+server.listen(port, () => {
+    console.log(`API running on port ${port}`)
+})
